d7a361d13ac288d00709f0eb9218dad2e09f63de
mkranioti <mar.kranioti@gmail.com>
2021-01-03 12:50:54 +0200

    minor changes

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          <h4>
+            fdffdfd
+          </h4>
         </p>
         <a
           className="App-link"
